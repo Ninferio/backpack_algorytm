55e2637ea2ba36e3a592545cf22c594508a141a5
prb10 <prb10>
2022-11-18 01:20:23 +0500

    add new feature

--- Backpack_algorytm/MainWindow.xaml.cs
+++ Backpack_algorytm/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace Backpack_algorytm
 {
@@ -27,19 +28,17 @@ namespace Backpack_algorytm
         List<Items> items = new List<Items>();
         int count;
         string path = @"Items\";
-        int max_space = 2000;
+ 
+        int max_space = 0;
         int[,] table;
         int matrix_height;
         int matrix_wight;
         public MainWindow()
         {
             InitializeComponent();
-            string[] ddd = null ;
+            
             Get_file_from_folder();
             //Get_items_from_file(cmb_files.Text);
-
-
-
 
         }
         public void GetItemCount(string path)
@@ -55,9 +54,10 @@ namespace Backpack_algorytm
 
         }
 
-        public void GoBackPack()
+        public void GoBackPack(Button button)
         {
-            
+            MessageBox.Show(button.Name);
+            GetItemCount(@"Items\\" + cmb_files.SelectedItem);
             matrix_height = count + 1;
             matrix_wight = max_space + 1;
             table = new int[matrix_height, matrix_wight];
@@ -78,13 +78,14 @@ namespace Backpack_algorytm
                     }
                     else
                     {
+                        //вес рюкзака
                         int backpack_weight = j;
-                        //вес, объем
+                        //вес, объем предмета
                         int area_item = items[i - 1].area;
-                        //стоимость, значимость
+                        //стоимость, значимость предмета
                         int value_item = items[i - 1].value;
                         //вес предмета меньше веса рюкзака?
-                        if (area_item < backpack_weight)
+                        if (area_item <= backpack_weight)
                         {
                             
                             //стоимость предмета с верху
@@ -101,22 +102,52 @@ namespace Backpack_algorytm
                             //берем стоимость предмета выше
                             table[i, j] = table[i - 1, j];
                         }
-
                     }
+                }
+            }
+            List<Items> enable_items = new List<Items>();
+            int k = matrix_wight-1;
+            //проходим по всем предметам с конца
+            while (k >= 0)
+            {
+                for (int i = count; i > 0; i--)
+                {
 
-
+                    if (table[i, k] != table[i - 1, k])
+                    {
+                        enable_items.Add(items[i - 1]);
+                        k -= items[i - 1].area;
+                        break;
+                    }
+                    else
+                    {
+                        if (table[i, k] == 0& table[i - 1, k] == 0)
+                        {
+                            k -= 1;
+                            break;
+                        }
+                    }
+                    
 
 
 
                 }
-
-
-
-
             }
-            GetMatrix(table);
+                
+            
+            
+                int all_value = 0;
 
-
+            int all_area = 0;
+            foreach (var a in enable_items)
+            {
+                all_value += a.value;
+                all_area += a.area;
+            }
+            MessageBox.Show(String.Format("Стоимость: {0} \n Площадь: {1}", all_value, all_area));
+            
+            dtg_select_items_table.ItemsSource = enable_items;
+                    //GetMatrix(table);
         }
 
         public void GetMatrix(int[,] Matrix)
@@ -132,50 +163,53 @@ namespace Backpack_algorytm
                 {
                     str += " "+ Matrix[i, j];
                 }
-                lblmatrix.Content += str + "\n";
+                //lblmatrix.Content += str + "\n";
             }
-            lblmatrix.Content += "=================" + "\n";
+            
+            //lblmatrix.Content += "=================" + "\n";
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            GoBackPack();
+            Button button = sender as Button;
+            switch (button.Name)
+            {
+                case "btn_Bounded_Knapsack_Problem":
+                    GoBackPack(button);
+                    btn_Unbounded_Knapsack_Problem.IsEnabled = false;
+                    btn_Continuous_Knapsack_Problem.IsEnabled = false;
+                    break;
+                case "btn_Unbounded_Knapsack_Problem":
+                    GoBackPack(button);
+                    btn_Bounded_Knapsack_Problem.IsEnabled = false;
+                    btn_Continuous_Knapsack_Problem.IsEnabled = false;
+                    break;
+                case "btn_Continuous_Knapsack_Problem":
+                    GoBackPack(button);
+                    btn_Unbounded_Knapsack_Problem.IsEnabled = false;
+                    btn_Bounded_Knapsack_Problem.IsEnabled = false;
+                    break;
+
+                case "btn_delete_result":
+                    btn_Continuous_Knapsack_Problem.IsEnabled = false;
+                    btn_Unbounded_Knapsack_Problem.IsEnabled = false;
+                    btn_Bounded_Knapsack_Problem.IsEnabled = false;
+                    break;
+            }
+            
         }
 
         private void Btn_delete_item_Click(object sender, RoutedEventArgs e)
         {
-            List<Items> select_items = new List<Items>();
-            foreach(var data in dtg_items_table.SelectedItems)
-            {
-                Items item =  data as Items;
-                select_items.Add(item);
-            }
-            //Удоление выбранных предметов
-            foreach (var item in select_items)
-            {
-                string item_string = String.Format("{0},{1},{2}", item.name, item.area, item.value);
-                //перепор файла
-                string line;
-                TextReader reader = new StreamReader(@"Items\\"+ cmb_files.SelectedItem.ToString());
-
-                while ((line = reader.ReadLine()) != null)
-                {
-                    
-                    if(line != item_string)
-                    {
-                     
-                    }
-                   
-                }
-                reader.Close();
-                
-            }
+            Items selected_item = (Items)dtg_items_table.SelectedItem;
+            //dtg_items_table.SelectedItem.
            
         }
 
         private void Btn_add_item_Click(object sender, RoutedEventArgs e)
         {
-           
+            Items selected_item = (Items)dtg_items_table.SelectedItem;
+
             //Window window = new Add_item_wondow();
             //window.Show();
             int max_id = items.Max(x => x.id) +1;
@@ -191,7 +225,7 @@ namespace Backpack_algorytm
         private void Dtg_items_table_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
         {
            
-            
+
             btn_save_change.IsEnabled = true;
             btn_delete_item.IsEnabled = true;
            
@@ -203,9 +237,34 @@ namespace Backpack_algorytm
         }
         private void Btn_save_change_Click(object sender, RoutedEventArgs e)
         {
-            Items selected_item = (Items)dtg_items_table.SelectedItem;
+           
+            List<Items> select_items = new List<Items>();
+            foreach (var data in dtg_items_table.SelectedItems)
+            {
+                Items item = data as Items;
+                select_items.Add(item);
+            }
+            //Удоление выбранных предметов
+            foreach (var item in select_items)
+            {
+                string item_string = String.Format("{0},{1},{2}", item.name, item.area, item.value);
+                //перепор файла
+                string line;
+                TextReader reader = new StreamReader(@"Items\\" + cmb_files.SelectedItem.ToString());
 
-            
+                while ((line = reader.ReadLine()) != null)
+                {
+
+                    if (line != item_string)
+                    {
+
+                    }
+
+                }
+                reader.Close();
+
+            }
+
         }
         /// <summary>
         /// Создание списка файлов
@@ -227,7 +286,7 @@ namespace Backpack_algorytm
                 i++;
             }
             cmb_files.ItemsSource = files;
-            cmb_files.SelectedItem = files[0];
+            cmb_files.SelectedItem = files[1];
             
            
 
@@ -281,6 +340,18 @@ namespace Backpack_algorytm
         {
             
             Get_items_from_file(cmb_files.SelectedItem.ToString());
+        }
+
+        private void Txb_backpakc_area_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void Txb_backpakc_area_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            max_space = Convert.ToInt32(txb_backpakc_area.Text);
+            
         }
     }
 }
